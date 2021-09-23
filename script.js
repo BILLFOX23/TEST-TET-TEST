0569f26ec9a35af8ecf71520d12f0f1a24b39fbd
BILLFOX23 <martin.mejia.iics@ust.edu.ph>
2021-09-23 10:44:00 +0800

    added script2,js

---
+++ script.js
@@ -0,0 +1,19 @@
+let info = function(firstName,lastName,age,subject){
+    let temp = {};
+    temp.firstName = firstName;
+    temp.lastName = lastName;
+    temp.age = age;
+    temp.subject = subject;
+    temp.introduce = function(){
+    console.log(`Hello, I am ${myself.firstName}
+    ${myself.lastName}.`);
+    }
+    temp.enrolled = function(){
+    console.log(`I am enrolled in ${myself.subject}`);
+    }
+    return temp
+    };
+    let myself = info("Juan", "dela Cruz", 20)
+    myself.introduce();
+    
+    myself.enrolled();
